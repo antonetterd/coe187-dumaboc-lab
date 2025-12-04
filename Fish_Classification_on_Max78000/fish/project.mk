ff27913a4176f6a25d5401cd0a8d117f6613d316
antonette <antonette.dumaboc@g.msuiit.edu.ph>
2025-12-04 13:50:55 +0800

    Add files via upload

---
+++ Fish_Classification_on_Max78000/fish/project.mk
@@ -0,0 +1,14 @@
+# Build configuration for fish CNN example on MAX78000
+
+# Root of the MaximSDK installation
+MAXIM_PATH := C:/MaximSDK
+
+# Target and board
+TARGET := MAX78000
+BOARD  := FTHR_RevA
+
+# Compiler
+COMPILER := GCC
+
+# Optional: name of the output ELF
+PROJECT := fish
---
+++ Fish_Classification_on_Max78000/fish/sampleoutput.h
@@ -0,0 +1,5 @@
+// This file was @generated automatically
+
+#define SAMPLE_OUTPUT { \
+  0x50401000, 0xffffffff, 0x00000003, 0x00007604, 0xfffa58e5, 0x0001eec1, 0x00000000 \
+}
